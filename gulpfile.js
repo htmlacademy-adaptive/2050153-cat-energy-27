a532df2843b9c9bdfb8e8574451eabd75bca66fe
Наталья Зиновьева <natalizin85@gmail.com>
2023-03-06 14:10:38 +1100

    автоматизация

--- gulpfile.js
+++ gulpfile.js
@@ -2,28 +2,65 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import sass from 'gulp-dart-sass';
 import postcss from 'gulp-postcss';
+import rename from 'gulp-rename';
+import csso from 'postcss-csso';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
 
 // Styles
 
-export const styles = () => {
-  return gulp.src('source/sass/style.scss', { sourcemaps: true })
-    .pipe(plumber())
-    .pipe(sass().on('error', sass.logError))
-    .pipe(postcss([
-      autoprefixer()
+export const styles = () => { // name
+  return gulp.src('source/sass/style.scss', { sourcemaps: true }) // 1. style.scss
+
+  .pipe(plumber()) // 2. обработка ошибок
+    .pipe(sass().on('error', sass.logError)) // style.scss -> style.css
+    .pipe(postcss([ // style.css
+      autoprefixer(), // style.css -> style.css[prefix]
+      csso()  // style.css[prefix] -> style.css[prefix, min]
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('build/css', { sourcemaps: '.' })) // 3. sourse/css
     .pipe(browser.stream());
 }
+
+// HTML
+
+const html = () => { // name
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+
+const script = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'));
+}
+
+// Images
+
+// const images = () => {
+//   return gulp.src('source/img/*/*.jpg.png')
+//     .pipe(terser())
+//     .pipe(gulp.dest('build/img'));
+// }
+
+// WebP
+
+
+
+//SVG
 
 // Server
 
 const server = (done) => {
   browser.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'build'
     },
     cors: true,
     notify: false,
@@ -41,5 +78,5 @@ const watcher = () => {
 
 
 export default gulp.series(
-  styles, server, watcher
+  html, styles, script, server, watcher
 );
